372c48b8d257aa34e2a0fe5c5f3e2ebd34451a41
Maisam Somji <maisam.somji@gmail.com>
2020-01-07 20:08:03 -0500

    list-ops

---
+++ elixir/list-ops/lib/list_ops.ex
@@ -0,0 +1,47 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
+
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+    length(l)
+  end
+
+  @spec reverse(list, acc) :: list
+  def reverse([], acc), do: acc
+  def reverse([h | t], acc \\ []), do: reverse(t, [h | acc])
+
+  @spec map(list, (any -> any)) :: list
+  def map([], _), do: []
+  def map([h | t], f), do: [f.(h) | map(t, f)]
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter([], _), do: []
+
+  def filter([h | t], f) do
+    if f.(h) do
+      [h | filter(t, f)]
+    else
+      filter(t, f)
+    end
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, (any, acc -> acc)) :: acc
+  def reduce([], acc, _), do: acc
+
+  def reduce([h | t], acc, f) do
+    reduce(t, f.(h, acc), f)
+  end
+
+  @spec append(list, list) :: list
+  def append([], a), do: a
+  def append([h | t], b), do: [h | append(t, b)]
+
+  @spec concat([[any]]) :: [any]
+  def concat([]), do: []
+  def concat([h | t]), do: append(h, concat(t))
+end
